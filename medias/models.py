abb4f00b57b27a2fe64d66fa7c0fb9476980e121
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-02-19 02:02:20 +0900

    medias

--- medias/models.py
+++ medias/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+
+class Photo(CommonModel):
+
+    file = models.URLField()
+    description = models.CharField(
+        max_length=140,
+    )
+    post = models.ForeignKey(
+        "posts.Post",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="photos",
+    )
+
+    def __str__(self):
+        return "Photo File"
+
+
+class Video(CommonModel):
+
+    file = models.URLField()
+
+    def __str__(self):
+        return "Video File"
