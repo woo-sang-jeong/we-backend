83f55332b2d8ac7560cdf2ea82236bf00c776240
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-02-19 10:40:24 +0900

    config/urls.py  revise

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -7,7 +9,10 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/posts/", include("posts.urls")),
+    path("api/categories", include("categories.urls")),
+    path("api/medias/", include("medias.urls")),
     path("api/users/", include("users.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
