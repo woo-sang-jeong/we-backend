27a29714e5f0bcaa4160b9e8cb308b5cdd9cc968
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-02-21 11:44:40 +0900

    local dev cors allowed

--- config/settings.py
+++ config/settings.py
@@ -163,3 +163,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
