6fd406d570d3cae8c9b726b241a5a17c8eceb428
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-02-21 12:01:00 +0900

    CSRF TRUSTED

--- config/settings.py
+++ config/settings.py
@@ -168,3 +168,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
