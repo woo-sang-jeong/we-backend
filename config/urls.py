ce31d9c30a37e8859e8f0929d357cf135e9c1a92
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-02-22 07:33:41 +0900

    SimpleJWT to users app

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,6 +10,4 @@ urlpatterns = [
     path("api/categories", include("categories.urls")),
     path("api/medias/", include("medias.urls")),
     path("api/users/", include("users.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import tokens
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("", views.Users.as_view()),
@@ -9,4 +13,6 @@ urlpatterns = [
     path("log-in", views.Login.as_view()),
     path("log-out", views.LogOut.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
